161b59a0b923d14ab6570fd9708bf6808371492d
István Z. Kovács <istvanzk@gmail.com>
2022-04-06 21:25:45 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,71 @@
+import os
+import platform
+import sys
+from pathlib import Path
+
+from pkg_resources import parse_version
+from setuptools import find_packages, setup
+
+
+def main():
+    version_file = Path(__file__).parent / 'VERSION'
+    with open(str(version_file)) as fin:
+        version = fin.read().strip()
+
+    install_requires_base = [
+        'absl-py',
+        'tf-models-official',
+        'sox',
+        'soundfile',
+    ]
+
+    decoder_pypi_dep = [
+        'ds_ctcdecoder == {}'.format(version)
+    ]
+
+    tensorflow_pypi_dep = [
+        'tensorflow >= 2.3.0'
+    ]
+    install_requires = install_requires_base + decoder_pypi_dep + tensorflow_pypi_dep
+
+    # TODO: Check if this is needed
+    #horovod_pypi_dep = [
+    #    'horovod[tensorflow] >= 0.21.3'
+    #]
+    #install_requires = install_requires + horovod_pypi_dep
+    
+
+    setup(
+        name='tf-deep_speech',
+        version=version,
+        description='Code for Deep Speech 2 based on Google /tensorflow/models/research/deep_speech',
+        url='https://github.com/istvanzk/tf-deep_speech',
+        author='Deep Speech 2 authors; Google deep_speech authors; Istvan Z. Kovacs',
+        license='Apache Software License 2.0',
+        # Classifiers help users find your project by categorizing it.
+        #
+        # For a list of valid classifiers, see https://pypi.org/classifiers/
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Topic :: Multimedia :: Sound/Audio :: Speech',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+        ],
+        #package_dir={'': 'training'},
+        packages=['tf-deep_speech'],
+        python_requires='>=3.8, <4',
+        install_requires=install_requires,
+        # If there are data files included in your packages that need to be
+        # installed, specify them here.
+        package_data={
+            'tf-deep_speech': [
+                'VERSION',
+            ],
+        },
+    )
+
+if __name__ == '__main__':
+    main()
