88750330fc5acc234fbeca25b69495c74b4cf086
Istvan Z. Kovacs <istvanzk@gmail.com>
2022-08-31 22:24:20 +0200

    Use as_numpy_iterator

--- trainer/ds2_train.py
+++ trainer/ds2_train.py
@@ -205,9 +205,10 @@ def run_deep_speech(_):
     input_dataset_test = dataset.input_fn(per_replica_batch_size, test_speech_dataset)
 
     # Get one element from the input dataset
-    #features = input_dataset_train.take(1)["features"]
-    input_length = input_dataset_train.take(1)["input_length"]
-    #label_length = input_dataset_train.take(1)["label_length"]
+    dict_data = list(input_dataset_train.take(1).as_numpy_iterator())[0]
+    input_length = dict_data["input_length"]
+    #features = dict_data["features"]
+    #label_length = dict_data["label_length"]
 
     # Use distribution strategy for multi-gpu training (when available)
     logging.info("Model generation and distribution...")
