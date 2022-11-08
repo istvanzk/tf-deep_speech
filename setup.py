ba9434c4ad3a6ebb240f38b90f6858a8ce32f66d
Istvan Z. Kovacs <istvanzk@gmail.com>
2022-11-08 20:32:38 +0100

    Clean-up

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 import platform
 import sys
 from pathlib import Path
-
-from pkg_resources import parse_version
 from setuptools import find_packages, setup
 
 
@@ -15,24 +13,14 @@ def main():
     install_requires_base = [
         'absl-py',
         'sox>=1.3.3',
-        'soundfile>=0.10.2',
         'nltk>=3.3',
         'pandas>=0.23.3'
     ]
 
     tensorflow_pypi_dep = [
-        'tf-models-official',
-        'tensorflow_cloud',
-        'tensorflow >= 2.8.0'
+        'tf-models-official==2.9.2',
     ]
     install_requires = install_requires_base + tensorflow_pypi_dep
-
-    # TODO: Check if this is needed
-    #horovod_pypi_dep = [
-    #    'horovod[tensorflow] >= 0.21.3'
-    #]
-    #install_requires = install_requires + horovod_pypi_dep
-    
 
     setup(
         name='tf-deep_speech',
@@ -53,23 +41,11 @@ def main():
             'Programming Language :: Python :: 3',
             'Operating System :: OS Independent',
         ],
-        #package_dir={'': 'trainer'},
+        # Install local packages
         packages=['trainer', 'model', 'data'],
-        python_requires='>=3.8, <4',
-        install_requires=install_requires,
-        # Install additional data files
-        data_files=[
-            ('.', [
-                'VERSION',
-                'run_ds2_train_cloud.sh',
-            ])
-        ],
-        # Install additional package data files
-        package_data={
-            'data':[
-                'vocabulary-hu.txt',
-            ]
-        },
+        #packages=find_packages(),
+        include_package_data=True,
+        python_requires='>=3.8, <3.10',
     )
 
 if __name__ == '__main__':
